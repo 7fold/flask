585da14db232e29d97105536c0c719622ea2e05c
root <root@yuriy.ca>
2018-11-20 02:48:41 -0500

    second commit

--- app/main.py
+++ app/main.py
@@ -1,69 +1,9 @@
-import os, mysql.connector
-from typing import List, Dict
-from flask import Flask, send_file, jsonify
-app = Flask(__name__)
+import connexion
+from flask import Flask
 
-
-@app.route("/")
-def hello():
-    return "Hello World from Flask in a uWSGI Nginx Docker container with \
-     Python 3.7 (from the example template)"
-
-@app.route("/test")
-def main():
-    index_path = os.path.join(app.static_folder, 'index.html')
-    return send_file(index_path)
-
-@app.route("/lol")
-def lol():
-    return "Lol world!"
-
-@app.route("/a")
-def a():
-    return "AAAA"
-
-@app.route('/json')
-def jsON():
-    data = {
-    "firstName": "Jane",
-    "lastName": "Doe",
-    "hobbies": ["running", "sky diving", "singing"],
-    "age": 35,
-    "children": [
-        {
-            "firstName": "Alice",
-            "age": 6
-        },
-        {
-            "firstName": "Bob",
-            "age": 8
-        }
-    ]
-}
-    return jsonify(data)
-
-def favorite_colors() -> List[Dict]:
-    config = {
-        'user': 'root',
-        'password': 'SomeStrongPassword123!',
-        'host': 'mysql',
-        'port': '3306',
-        'database': 'knights'
-    }
-    connection = mysql.connector.connect(**config)
-    cursor = connection.cursor()
-    cursor.execute('SELECT * FROM favorite_colors')
-    results = [{name: color} for (name, color) in cursor]
-    cursor.close()
-    connection.close()
-
-    return results
-
-@app.route("/db")
-def index() -> str:
-    return jsonify({'favorite_colors': favorite_colors()})
-
+app = connexion.FlaskApp(__name__, specification_dir="./")
+app.add_api('swagger.yml')
 
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(debug=True)
